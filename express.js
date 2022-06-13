5f712660d01420fb10b5364d95c4d8fc7a79dbb5
buckybass <bassjibass@gmail.com>
2022-06-13 19:45:50 +0700

    user params

--- express.js
+++ express.js
@@ -6,12 +6,26 @@ app.get('/', (req, res) => {
   res.end()
 })
 
-app.get('/404', (req, res) => {
-  res.status(404).send('Not found 404')
-})
+const user = [
+  {name:'jordi',age:18},
+  {name:'bucky',age:30},
+  {name:'steve',age:30},
+  {name:'tony',age:35}
+]
 
-app.get('/403', (req, res) => {
-  res.status(403).send('Not Allow')
+app.get('/users/:id',(req,res)=>{
+  const users = user[req.params.id - 1]
+  if(Number.isNaN(+req.params.id)){
+    return res.status(400).send('Id is not Number')
+  }
+  if(req.params.id<=0){
+    return res.status(400).send('id is nagetive')
+  }
+  if(!users){
+    return res.status(404).send({ error: 'Not found'})
+  }else{
+    return res.send(users)
+  }
 
 })
 
