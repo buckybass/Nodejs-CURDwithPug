cb56dd2f079a5e0cc554128246678dcce870cd93
buckybass <bassjibass@gmail.com>
2022-06-21 21:50:19 +0700

    fix upload avatar

--- utils/uploadAvatar.js
+++ utils/uploadAvatar.js
@@ -3,10 +3,12 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = async(req,id) => {
-  await sharp(req.file.path)
-    .resize(200, 200)
-    .jpeg({ quality: 70 })
-    .flatten({ background:'#fff'})
-    .toFile(path.join(__dirname, `../public/uploads/${id}.jpg`))
-  await fs.promises.unlink(req.file.path)
+  if(req.file){
+    await sharp(req.file.path)
+      .resize(200, 200)
+      .jpeg({ quality: 70 })
+      .flatten({ background:'#fff'})
+      .toFile(path.join(__dirname, `../public/uploads/${id}.jpg`))
+    await fs.promises.unlink(req.file.path)
+  }
 }
