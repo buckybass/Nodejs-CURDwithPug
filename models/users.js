5e40a9406b4f8cb07353e3b52abd79967a5b76e8
buckybass <bassjibass@gmail.com>
2022-06-23 21:33:34 +0700

    validation mogoose

--- models/users.js
+++ models/users.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    name:String,
-    age:Number,
+    name:{
+      type:String,
+      required:true,
+      trim:true,
+      maxlength:[100,'ชื่อต้องไม่เกิน 100 ตัว']
+    },
+    age:{
+      type:Number,
+      required:true,
+      min:[1,'อายุต้องมากว่า 1'],
+      max:[150,'อายุต้องไม่เกิน 150']},
     avatar:String
-})
+},{timestamps:true})
 
 module.exports=mongoose.model('Users',schema)
